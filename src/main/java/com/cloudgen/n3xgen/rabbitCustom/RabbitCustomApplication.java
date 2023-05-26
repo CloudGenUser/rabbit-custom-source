792dfa8385b617eaf408ae06a5f01d216e176af8
Alejandro Martinez <Alejandro.Martinez@cloudgensys.com>
2023-05-26 13:40:56 -0500

    se sube la aplicacion de rrabit completa al github

--- src/main/java/com/cloudgen/n3xgen/rabbitCustom/RabbitCustomApplication.java
+++ src/main/java/com/cloudgen/n3xgen/rabbitCustom/RabbitCustomApplication.java
@@ -1,13 +1,13 @@
-package com.cloud.n3xgen.httpcustom;
+package com.cloudgen.n3xgen.rabbitCustom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HttpCustomApplication {
+public class RabbitCustomApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HttpCustomApplication.class, args);
+		SpringApplication.run(RabbitCustomApplication.class, args);
 	}
 
 }
